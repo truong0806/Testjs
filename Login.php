0c48d015e915a04b7c039d22ed3741d15e62b267
truong0806 <thanhtruong090123@gmail.com>
2021-03-15 21:19:13 +0700

    master

---
+++ Login.php
@@ -0,0 +1,9 @@
+<?php
+
+if(isset($_POST('uname')) && isset('password')){
+    function validate($data)
+    $uname = $_POST['uname'];
+    $pass = $_POST['password'];
+}else{
+    header("Location: index.php")
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Docum    ent</title>
+</head>
+<body>
+ <form action="Login.php" metho="post">
+    <h2>Đăng nhập</h2>
+        <label>Tên đăng nhập</label>
+        <input type="text" name= "uname" placeholder = "Username"><br>
+        
+        <label>Mật khẩu</label>
+        <input type="text" name= "password" placeholder = "Password"><br>
+        <a href="">Quên mật khẩu</a>
+        <button type = "submit">Đăng nhập</button>
+    </form>
+</body>
+</html>
